7955672fa541d5d126b47acbc8018b0e0216f376
DanceTherapeutics <dancetherapeutics@gmail.com>
2017-10-18 14:25:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logWhisper(string){
+  console.log('hello')
+}
+function logShout(string){
+  console.log('HELLO')
+}
+var string = lowercase
+if (string === 'hello'){
+function sayHiToGrandma(string){
+  return("I can't hear you!")
+} else if {
+    (string === 'HELLO'){
+  function sayHiToGrandma(string){
+  return("YES INDEED!")}
+} else {
+function sayHiToGrandma(string){
+  return("I love you, too!")}
+}
